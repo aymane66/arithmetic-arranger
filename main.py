dbac96a4d6de810f357a99c94e10ba33b1883bb4
aymane66 <ayman.mouhcine1995@gmail.com>
2023-12-15 03:07:28 +0100

    main file

---
+++ main.py
@@ -0,0 +1,4 @@
+from arithmetic_arranger import arithmetic_arranger
+
+# Change the numbers to perform different math operations.
+print(arithmetic_arranger(["30 + 8", "3801 - 2", "45 + 43", "123 + 49"]))
